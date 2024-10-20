bdd8767b155ea95a97ba1e19f49b020f2343c2c9
Teagan <41862957+teaganbuckley@users.noreply.github.com>
2024-10-21 08:58:17 +1100

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('🤖 Machine Learning App', icon=":material/thumb_up:")
+st.title('🤖 Machine Learning App')
 
 st.info('This app builds a machine learning model.')
 
